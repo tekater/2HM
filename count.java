51af35dc8c05b56a4e9756cb2d6deae656018c1b
tekater <120014627+tekater@users.noreply.github.com>
2023-01-08 17:42:49 +0900

    Update count.java

--- count.java
+++ count.java
@@ -8,7 +8,7 @@ public class Main {
           int size = 3;
           int count = 0;
           int[] myArray = new int[size];
-
+        ///
         for (int num = 0; num < size; num++) {
             myArray[num] = scanner.nextInt();
         }
@@ -18,14 +18,12 @@ public class Main {
                 ++count;
             }
         }
-
-        for (int g = 0; g < count; g++) {
         ///
+        for (int g = 0; g < count; g++) {
             if (count == 1) {
                 System.out.println("true");
             }
-        }
-            if (count > 1) {
+        }   if (count > 1) {
                 System.out.println("false");
             }
         //if((num1 && num2 == 1) || (num3 && num2 == 1) || (num1 && num3){
